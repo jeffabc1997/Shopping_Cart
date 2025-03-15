b95d5f9a10dce7c0dbd5899add420626ce664336
jeffabc1997 <jeff.tsai86@gmail.com>
2025-03-15 11:53:02 +0800

    加入game, team, player, pitcher, hitter等資料庫

---
+++ baseballapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from baseballapp.models import PlayerModel, PitcherModel, HitterModel, TeamModel, GameModel
+# Register your models here.
+admin.site.register(PlayerModel)
+admin.site.register(PitcherModel)
+admin.site.register(HitterModel)
+admin.site.register(TeamModel)
+admin.site.register(GameModel)
---
+++ baseballapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BaseballappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'baseballapp'
