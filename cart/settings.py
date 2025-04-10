6111d34a9c03e7380a674b52fda8de0f72ec58d4
jeffabc1997 <jeff.tsai86@gmail.com>
2025-04-10 10:12:58 +0800

    調整login網址和db位置

--- cart/settings.py
+++ cart/settings.py
@@ -81,7 +81,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'), #BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #BASE_DIR / 'db.sqlite3',
     }
 }
 
